ae87ef75a7e9b8e9e7e394869e23746707baa737
emmettsrm <emmettsrm@gmail.com>
2020-01-23 10:43:12 +0200

    button

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 let changeColor = document.getElementById('scan-button');
 
+console.log("js file")
+
 changeColor.onclick = function(element) {
     console.log(window.location.href)
     let color = element.target.value;
