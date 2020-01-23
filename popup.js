f0e49a9707e8ef8270056ac16b5163414f60aa43
emmettsrm <emmettsrm@gmail.com>
2020-01-23 14:26:31 +0200

    post request

--- popup.js
+++ popup.js
@@ -3,9 +3,20 @@
 function findImg(){
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
-        alert(url);
-    });
-}
+        var jsonUrl = JSON.stringify(url);
+        alert(jsonUrl);
+        fetch('/hello', {
+            method: 'POST',
+            body: jsonUrl
+            })
+        }).then(function (response) {
+            return response.text();
+        }).then(function (text) {
+            alert('POST response: ' + text);
+        });
+
+};
+
 
 document.addEventListener("DOMContentLoaded", function(){
     scanBtn = document.getElementsByTagName('button')[0];
