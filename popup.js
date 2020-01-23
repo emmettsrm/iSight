22e80b1534943cd1306c43913032f5285704ab3d
emmettsrm <emmettsrm@gmail.com>
2020-01-23 13:26:33 +0200

    kjh

--- popup.js
+++ popup.js
@@ -9,17 +9,3 @@ document.addEventListener("DOMContentLoaded", function(){
     alert(scanBtn)
     scanBtn.addEventListener('click', findImg);
   });
-// chrome.storage.sync.get('color', function(data) {
-//     scannedDoc.style.backgroundColor = data.color;
-//     scannedDoc.setAttribute('value', data.color);
-//   });
-
-// scannedDoc.onclick = function(element) {
-//     console.log(window.location.href)
-//     let color = element.target.value;
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//     });
-//   };
