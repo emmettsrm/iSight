d3df42c5bd5bdd32870a95d6d11edf6e9d1db6af
emmettsrm <emmettsrm@gmail.com>
2020-01-23 11:52:46 +0200

    button

---
+++ js/popup.js
@@ -0,0 +1,31 @@
+// let scannedDoc = document.getElementById('scan-button')
+// console.log(scannedDoc)
+
+// scannedDoc.addEventListener('click', findImg())
+
+// function findImg(){
+//     console.log("button works")
+// }
+
+function hello() {
+  chrome.tabs.executeScript({
+    file: 'popup.js'
+  }); 
+}
+
+document.getElementById('scan-button').addEventListener('click', hello);
+
+// chrome.storage.sync.get('color', function(data) {
+//     scannedDoc.style.backgroundColor = data.color;
+//     scannedDoc.setAttribute('value', data.color);
+//   });
+
+// scannedDoc.onclick = function(element) {
+//     console.log(window.location.href)
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
