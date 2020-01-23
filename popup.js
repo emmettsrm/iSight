03607dd692a011608ba8c686da91fcf4d562097a
emmettsrm <emmettsrm@gmail.com>
2020-01-23 10:46:20 +0200

    button

--- popup.js
+++ popup.js
@@ -2,6 +2,11 @@ let changeColor = document.getElementById('scan-button');
 
 console.log("js file")
 
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+  });
+
 changeColor.onclick = function(element) {
     console.log(window.location.href)
     let color = element.target.value;
